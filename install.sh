65a29416fc042e52ee5ff3b46fc9597c0868106c
amiralimi <amirali.mirian@gmail.com>
2025-07-30 01:14:28 -0400

    added install script. for now im just saving the stuff. will update it later.

---
+++ install.sh
@@ -0,0 +1,7 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+brew install zsh-autosuggestions zsh-syntax-highlighting
+brew install fzf fd bat
+
+git submodule update --init --recursive
+
