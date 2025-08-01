2afe8482700308dad6cd41261225e3514d2cf878
amiralimi <amirali.mirian@gmail.com>
2025-08-01 00:46:59 -0400

    updated install. note: for yazi, i copied the whole command.

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ brew install fzf fd bat
 git submodule update --init --recursive
 
 brew install zoxide
+brew install micro
+brew install --cask font-iosevka-nerd-font
+brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font
+brew install stow
+
+stow -t ~/.config .config
